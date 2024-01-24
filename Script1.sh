491bd0325f4f181089ee8962f5eb93aefd63173c
Varsha Salve <103528654+varshusalve@users.noreply.github.com>
2024-01-24 21:22:04 +0530

    Add files via upload

---
+++ Script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
+echo "Hello batch-21, We are learning Jenkins"
+echo "Good morning batch-22, We are learning GitHub"
+echo "We are learning branching strategies"
+
